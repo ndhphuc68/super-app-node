4ba7b1fb5a4dfb243658dfb87735c412e73dc2ab
ndhphuc <ndanghoangphuc@gmail.com>
2023-07-25 18:15:03 +0700

    aaaa

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const helmet = require("helmet");
 const morgan = require("morgan");
 
 // defining the Express app
 const app = express();
-
-// adding Helmet to enhance your API's security
-app.use(helmet());
 
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
